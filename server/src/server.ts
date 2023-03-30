2266ba3d3addb5536ec2ae4b38be7b5d8bbe667e
Heleno Salgado <73316162+HelenoSalgado@users.noreply.github.com>
2023-03-30 17:49:17 -0300

    Populando tabela serviços.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 const server = express();
 dotenv.config();
 const PORT = process.env.PORT;
 
-console.log(PORT);
-
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+}
+server.use(cors(corsOptions));
 server.use(express.json());
 server.use('/api/v1/', require('./routes/barberPublicRoutes'));
 server.use('/api/v1/dashboard', require('./routes/barberAdminRoutes'));
