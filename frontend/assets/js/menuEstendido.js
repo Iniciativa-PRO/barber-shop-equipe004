380552f0da0d4ca0f730544a017ef76ac57a892f
Heleno Salgado <73316162+HelenoSalgado@users.noreply.github.com>
2023-03-31 11:24:08 -0300

    Testando.

--- frontend/assets/js/menuEstendido.js
+++ frontend/assets/js/menuEstendido.js
@@ -1,19 +1,23 @@
 let menuHamburger = document.getElementById('jsmenu-hamburger');
 let menuEstendido = document.querySelectorAll('.jsanimation');
 
-menuHamburger.addEventListener('click', () => {
+function menuActive() {
     document.getElementById('jsmenu-estendido').classList.toggle('menu-estendido-visible');
-
     menuHamburger.classList.toggle('menu-hambuger-lines');
-});
+};
+
+menuHamburger.addEventListener('click', menuActive);
 
 menuEstendido.forEach((link) => {
     link.addEventListener('mousemove', () => animationLink(link));
-    link.addEventListener('mouseout', () => animationLinkRemove(link))
+    link.addEventListener('mouseout', () => animationLinkRemove(link));
+    link.addEventListener('click', () => menuActive());
 });
+
 function animationLink(link) {
     return link.classList.add('menu-estendido-li-animation');
 }
+
 function animationLinkRemove(link) {
     return link.classList.remove('menu-estendido-li-animation');
 }
